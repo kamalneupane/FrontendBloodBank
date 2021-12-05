74e8add6874fefd145df16f81365fed03f2cbd4b
Kamal Neupane <kamal12neupane@gmail.com>
2021-12-05 11:09:58 +0545

    unhandled promise handled

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -3,9 +3,9 @@ const ErrorHandler = require('../utils/errorHandler')
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     if(process.env.NODE_ENV === 'DEVELOPMENT'){
+        
         res.status(err.statusCode).json({
             success: false,
-            error: err,
             errMessage: err.message,
             stack: err.stack
         })
@@ -13,6 +13,14 @@ module.exports = (err, req, res, next) => {
     if(process.env.NODE_ENv === 'PRODUCTION'){
         let error = {...err}
         error.message = err.message
+
+        // wrong Mongoose object id error
+        if(err.name === 'CastError'){
+            const message = `Resource not found. Invalid ${err.path}`;
+            error = new ErrorHandler(message, 400);
+        }
+        
+
         res.status(error.statusCode).json({
             success: false,
             message: error.message || 'Internal Server Error'
--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const app = require('./app')
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database')
 
+// handle uncaught exception
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`)
+    console.log('shutting down server due to uncaught exception')
+    process.exit(1)
+})
+
+
 // config file
 dotenv.config({ path: 'config/config.env'})
 
@@ -11,6 +19,14 @@ connectDatabase();
 
 
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+})
+// handle unhandled promise rejection
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err}`)
+    console.log('Shutting down the server due to unhandled promise rejection')
+    server.close(() => {
+        process.exit(1)
+    })
 })
